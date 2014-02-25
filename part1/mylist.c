77a8ab9ce352c202a78ca8edbfb83a48ec42fc50
Elisha Lee <esl2131@columbia.edu>
2014-02-25 01:35:51 -0500

    Started basics of part1 and part2 and made Makeme

---
+++ part1/mylist.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mylist.h>
+
+struct Node *addFront(struct List *list, void *data) 
+{
+
+}
+
+void traverseList(struct List *list, void (*f)(void *))
+{
+
+}
+
+void flipSignDouble(void *data)
+{
+
+}
+
+int compareDouble(const void *data1, const void *data2)
+{
+
+}
+
+struct Node *findNode(struct List *list, const void *dataSought,
+        int (*compar)(const void *, const void *))
+{
+
+}
+
+void *popFront(struct List *list) 
+{
+
+}
+
+void removeAllNodes(struct List *list)
+{
+
+}
+
+struct Node *addAfter(struct List *list, 
+        struct Node *prevNode, void *data)
+{
+
+}
+
+void reverseList(struct List *list)
+{
+
+}
