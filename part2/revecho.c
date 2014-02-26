be8de1b578096219d777517255ce31e74a6258b8
Elisha Lee <esl2131@columbia.edu>
2014-02-25 22:39:38 -0500

    created and printed list

--- part2/revecho.c
+++ part2/revecho.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "mylist.h"
+
+int main(int argc, char **argv) {
+    struct List stringList;
+    int i;
+    initList(&stringList);
+    for(i = 1; i < argc; i++) {
+        addFront(&stringList, argv[i]);
+    }
+
+    struct Node *temp = stringList.head;
+    while (temp) {
+        printf("%s\n", (char *)temp->data);
+        temp = temp->next;
+    }
+    return 0;
+
+}
