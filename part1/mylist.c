ed4344331c21794bfc7954c202988f98ffb3411d
Elisha Lee <esl2131@columbia.edu>
2014-02-25 19:55:42 -0500

    wrote flipsigndouble

--- part1/mylist.c
+++ part1/mylist.c
@@ -2,37 +2,45 @@
 #include <stdlib.h>
 #include "mylist.h"
 
+
+// adds node to front of list
 struct Node *addFront(struct List *list, void *data) 
 {
+    //creates new node
     struct Node *front = (struct Node *)malloc(sizeof(struct Node *));
     if (front == NULL) 
     {
         perror("malloc returned NULL");
         exit(1);
     }
-
+    
+    //adds to front
     front->next = list->head;
     list->head = front;
     front->data = data;
     return front;
 }
 
-
+//calls f() on each element of the list
 void traverseList(struct List *list, void (*f)(void *))
 {
    struct Node *linkedList = list->head;
+   //traverses until list is null
    while (linkedList) {
        f(linkedList->data);
        linkedList = linkedList->next;
    }
 }
 
-/*
+
 void flipSignDouble(void *data)
 {
+    double *nodeData = (double *) data;
+    *nodeData = *nodeData * -1;
 
 }
 
+/*
 int compareDouble(const void *data1, const void *data2)
 {
 
