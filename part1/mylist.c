feb885ef038f32ed4c9c673a536ff5043a8edcfd
Elisha Lee <esl2131@columbia.edu>
2014-02-25 19:41:57 -0500

    edited Makefile, wrote traverse and add front

--- part1/mylist.c
+++ part1/mylist.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mylist.h>
+#include "mylist.h"
 
 struct Node *addFront(struct List *list, void *data) 
 {
+    struct Node *front = (struct Node *)malloc(sizeof(struct Node *));
+    if (front == NULL) 
+    {
+        perror("malloc returned NULL");
+        exit(1);
+    }
 
+    front->next = list->head;
+    list->head = front;
+    front->data = data;
+    return front;
 }
+
 
 void traverseList(struct List *list, void (*f)(void *))
 {
-
+   struct Node *linkedList = list->head;
+   while (linkedList) {
+       f(linkedList->data);
+       linkedList = linkedList->next;
+   }
 }
 
+/*
 void flipSignDouble(void *data)
 {
 
@@ -48,3 +64,4 @@ void reverseList(struct List *list)
 {
 
 }
+*/
