b4e142f3a1a69f0f3f354e868a7f1a073de9303c
Elisha Lee <esl2131@columbia.edu>
2014-02-26 00:00:35 -0500

    added comments to revecho.c

--- part2/revecho.c
+++ part2/revecho.c
@@ -1,3 +1,12 @@
+/*
+ * Name: Elisha Lee
+ * UNI: esl2131
+ * File: revecho.c
+ * Description: Takes input from command line and inserts
+ * them into a linked list; prints out list and checks if "dude"
+ * is contained in the list"
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,10 +16,12 @@ int main(int argc, char **argv) {
     struct List stringList;
     int i;
     initList(&stringList);
+    // added strings from command line to linked list
     for(i = 1; i < argc; i++) {
         addFront(&stringList, argv[i]);
     }
-
+    
+    // prints list and checks if "dude" is in list
     struct Node *temp = stringList.head;
     int found = 0;
     while (temp) {
